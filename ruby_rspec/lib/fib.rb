c02a0a8089984c736162d3fffa996f106671f34a
andreAmorimF <andre.amorimFonseca@gmail.com>
2012-03-07 18:29:58 +0100

    - Deleting overnumbered examples.

--- ruby_rspec/lib/fib.rb
+++
@@ -1,6 +0,0 @@
-# Calc the fibonacci sequence
-# f(0) = 0 , f(1) = 1
-# f(n) = f(n-1) + f(n-2)
-
-def fib(number)
-end
--- ruby_rspec/lib/fib_solution.rb
+++
@@ -1,9 +0,0 @@
-# Calc the fibonacci sequence
-# f(0) = 0 , f(1) = 1
-# f(n) = f(n-1) + f(n-2)
-
-def fib(number)
-  return number if number.zero? or number == 1
-
-  fib(number-1) + fib(number-2)
-end
