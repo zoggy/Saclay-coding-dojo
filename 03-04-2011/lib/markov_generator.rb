0071940a1cf932010698aea457be176845ee5344
andreAmorimF <andre.amorimFonseca@gmail.com>
2012-04-03 16:51:06 +0200

    - Snpashot of session 03-04-2012. Problem not yet solved.

--- 03-04-2011/lib/markov_generator.rb
+++ 03-04-2011/lib/markov_generator.rb
@@ -1,3 +1,41 @@
-
 class MarkovGenerator
+  
+  private
+  
+  def add_word word 
+    @transitions[@prev_word] ||= {word => 0}
+    @transitions[@prev_word][word] ||= 0
+    @transitions[@prev_word][word] += 1
+    @prev_word = word
+  end
+  
+  def compute_frequencies
+    freq_array = @transitions.map { |key, value|
+          sum = 0.0
+          value.each { |k, v| sum += v }
+          [key, value.map { |k, v| [v / sum, k] }.sort]
+      }
+    @frequencies = Hash[freq_array]
+  end
+  
+  public
+  
+  def add_text text
+    text.scan(/\.|\?|\!|\w+/).each {|item| add_word(item)}
+    compute_frequencies
+  end
+  
+  def get_words(word)
+    @transitions[word] || {}
+  end
+  
+  def get_frequencies(word)
+    @frequencies[word] || {}
+  end
+    
+  def initialize 
+    @prev_word = ""
+    @transitions = {}
+    @frequencies = {}
+  end
 end
--- 03-04-2011/spec/markov_generator_spec.rb
+++ 03-04-2011/spec/markov_generator_spec.rb
@@ -3,4 +3,27 @@ require 'markov_generator'
 
 describe MarkovGenerator do
   
+  it "should add words with its correspondent frequence" do
+    text = "The man is tall. The man is big."
+
+    mc = MarkovGenerator.new
+    mc.add_text(text)
+    mc.get_words("is").should == {"big" => 1, "tall" => 1}
+  end
+  
+  it "should consider dots as a word" do
+    text = "The man is tall. The man is big."
+    mc = MarkovGenerator.new
+    mc.add_text(text)
+    mc.get_words("tall").should == {"." => 1}
+  end
+
+  it "should compute frequencies from counters" do
+    text = "The man is tall. The man is big."
+    
+    mc = MarkovGenerator.new
+    mc.add_text(text)
+    mc.get_frequencies("is").should == [[0.5, "big"], [0.5,"tall"]]
+  end
+  
 end
